0a54a6bad36ea5c0d27a652f55158bb12d1351ea
krushika Polra <kpolara548@gmail.com>
2023-07-16 16:17:00 +0530

    plugin

--- lib/plugin_screen/plugin_demo.dart
+++ lib/plugin_screen/plugin_demo.dart
@@ -1,4 +1,3 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class PluginDemo extends StatefulWidget {
@@ -12,38 +11,8 @@ class _PluginDemoState extends State<PluginDemo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            CarouselSlider(
-              items: [
-                Container(
-                  height: 500,
-                  width: 500,
-                  color: Colors.purpleAccent,
-                  child: Text("hello"),
-                ),
-              ],
-              options: CarouselOptions(
-                height: 400,
-                aspectRatio: 16 / 9,
-                viewportFraction: 0.8,
-                initialPage: 0,
-                enableInfiniteScroll: true,
-                reverse: false,
-                autoPlay: true,
-                autoPlayInterval: Duration(seconds: 3),
-                autoPlayAnimationDuration: Duration(milliseconds: 800),
-                autoPlayCurve: Curves.fastOutSlowIn,
-                enlargeCenterPage: true,
-                enlargeFactor: 0.3,
-                onPageChanged: (index, reason) {},
-                scrollDirection: Axis.horizontal,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+        body: Column(
+      children: [],
+    ));
   }
 }
